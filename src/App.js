110d716b5cefc5e3dc583b42aa9c9fa8dea4a6af
k.dubov <email@example.email>
2019-04-12 17:12:43 +0300

    displa zip code on click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const WeatherDisplay = (props) => {
-  return (
-    <h1>Displaying weather for city {props.zip}</h1>
-  );
+const PLACES = [
+  { name: "Minsk", zip: "94303" },
+  { name: "Moscow", zip: "94088" },
+  { name: "St-Petersberg", zip: "95062" },
+  { name: "Kiev", zip: "96803" }
+];
+
+class WeatherDisplay extends Component {
+  render() {
+    return (
+      <h1>Displaying weather for city {this.props.zip}</h1>
+    );
+  }
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    }
+  }
   render() {
+    const activePlace = this.state.activePlace;
     return (
       <div className="App">
-        <WeatherDisplay zip={"12345"} />
+        {PLACES.map((place, index) => (
+          <button key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}>
+            {place.name}
+          </button>
+        ))}
+        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
       </div>
     );
   }
